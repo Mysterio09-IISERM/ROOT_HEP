183b503733693472f9b7dd222b2b8ce3c8d4cece
Mysterio09-IISERM <ms23026@iisermohali.ac.in>
2026-05-25 11:41:37 +0530

    Made a Macro to give me fitted datasets of the third branch isn the mixed_fit_data.root file

---
+++ C_files/Mixedfitdata3.C
@@ -0,0 +1,185 @@
+    #include "TFile.h"
+    #include "TTree.h"
+    #include "TH1F.h"
+    #include "TCanvas.h"
+    #include "TROOT.h"
+    #include "TRandom.h"
+    #include "TStyle.h"
+    #include "TF1.h"
+    #include "TMath.h"
+
+    #include "RooRealVar.h"
+    #include "RooDataHist.h"
+    #include "RooBreitWigner.h"
+    #include "RooExponential.h"
+    #include "RooAddPdf.h"
+    #include "RooPlot.h"
+    #include "RooArgSet.h"
+    #include "RooGlobalFunc.h"
+    #include "RooChebychev.h"
+    #include <fstream>
+
+    using namespace RooFit;
+
+void Mixedfitdata3(){
+
+    //OPEN ROOT FILE
+
+    TFile *f = new TFile("../Root_files/mixed_fit_data.root", "READ");
+    TTree *t = (TTree*)f->Get("data");
+
+    //CREATE HISTOGRAMS
+
+    TH1F *h1 = new TH1F("h1", "Generated Distribution; Var; Counts", 100, 20.0, 80.0);
+    t->Draw("doublegauss_poly >>h1", "", "goff");
+    h1->SetDirectory(0);
+    f->Close();
+    delete f;
+
+    // ROOFIT VARIABLE
+
+    RooRealVar var("doublegauss_poly", "Variable", 20.0, 80.0);
+
+    // IMPORT DATA
+
+    RooDataHist data("data", "dataset", var, Import(*h1));
+
+    //SIGNAL PARAMETERS
+
+    RooRealVar mean1("mean1", "Mean 1", 40.0, 35.0, 55.0);
+    RooRealVar sigma1("sigma1", "Width 1", 2.5, 0.1, 10.0);
+    RooRealVar mean2("mean2", "Mean 2", 45.0, 35.0, 60.0);
+    RooRealVar sigma2("sigma2", "Width 2", 5.0, 0.1, 10.0);
+
+    // BACKGROUND PARAMETERS
+
+    RooRealVar c0("c0", "Cheby Coeff 0", 0.0, -1.0, 1.0);
+    RooRealVar c1("c1", "Cheby Coeff 1", 0.0, -1.0, 1.0);
+
+    // SIGNAL AND BACKGROUND PDFS
+
+    RooGaussian signal1("signal1", "Signal PDF", var, mean1, sigma1);
+    RooGaussian signal2("signal2", "Signal PDF 2", var, mean2, sigma2);
+    RooRealVar frac("frac", "Fraction of Signal 1", 0.5, 0.0, 1.0);
+    RooAddPdf signal("signal", "Combined Signal", RooArgList(signal1, signal2), frac);
+    RooChebychev background("background", "Background PDF", var, RooArgList(c0, c1));
+
+    // YEILD VARIABLES
+
+    RooRealVar nsig("nsig", "Signal Yield", 58000, 10000, 150000);
+    RooRealVar nbkg("nbkg", "Background Yield", 55000, 10000, 150000);
+
+    // COMBINED MODEL
+
+    RooAddPdf model("model", "Signal + Background Model", RooArgList(signal, background), RooArgList(nsig, nbkg));
+
+    //FIT MODEL TO DATA
+
+    model.fitTo(data);
+
+    //CANVAS CREATION
+
+    TCanvas *cmain = new TCanvas("cmain", "Fit Result", 1800, 800);
+
+    cmain->Divide(3,1);
+
+    // RAW DISTRIBUTION
+
+    cmain->cd(1);
+    RooPlot *frame1 = var.frame();
+    data.plotOn(frame1, Name("data"));
+    frame1->SetTitle("Raw Distribution");
+    frame1->Draw();
+
+    TLegend *leg1 = new TLegend(0.6, 0.85, 0.9, 0.9);
+    leg1->AddEntry(frame1->findObject("data"), "Data", "ep");
+    leg1->Draw();
+
+
+    // FITTED DISTRIBUTION
+
+    cmain->cd(2);
+    RooPlot *frame2 = var.frame();
+    data.plotOn(frame2, Name("data2"));
+    model.plotOn(frame2, Name("fullfit"));
+    model.plotOn(frame2, Components(background), LineColor(kGreen), Name("bkg"));
+    model.plotOn(frame2, Components(signal), LineColor(kRed), Name("sig"));
+    frame2->SetTitle("Fitted Distribution");
+    frame2->Draw();
+
+    TLegend *leg2 = new TLegend(0.6, 0.65, 0.9, 0.9);
+    leg2->AddEntry(frame2->findObject("data2"), "Data", "ep");
+    leg2->AddEntry(frame2->findObject("fullfit"), "Full Fit","l");
+    leg2->AddEntry(frame2->findObject("sig"), "Signal PDF", "l");
+    leg2->AddEntry(frame2->findObject("bkg"), "Background PDF", "l");
+    leg2->Draw();
+
+    // EXTRACTED SIGNAL COMPONENT
+
+    cmain->cd(3);
+    TH1F *hsig = (TH1F*)h1->Clone("hsig");
+    hsig->SetTitle("Extracted Signal Data; Var; Counts");
+
+    RooArgSet normSet(var);
+
+    for (int i=1; i<=hsig->GetNbinsX(); i++){
+        double x = hsig->GetBinCenter(i);
+        double bw = hsig->GetBinWidth(i);
+        var.setVal(x);
+        double bkg_density = background.getVal(normSet);
+        double bkg_counts = bkg_density * nbkg.getVal() * bw;
+        double y = hsig->GetBinContent(i);
+        hsig->SetBinContent(i, y - bkg_counts);
+    }
+
+    hsig->SetLineColor(kBlue);
+    hsig->SetMarkerStyle(20);
+    
+
+    TF1 *fsig1 = new TF1("fsig1", "gaus", 20.0, 80.0);
+    double scale1 = nsig.getVal() * hsig->GetBinWidth(1) * frac.getVal() / (sigma1.getVal() * sqrt(2 * TMath::Pi()));
+    double scale2 = nsig.getVal() * hsig->GetBinWidth(1) * (1 - frac.getVal())/(sigma2.getVal() * sqrt(2 * TMath::Pi()));
+    fsig1->SetParameters(scale1, mean1.getVal(), sigma1.getVal());
+    TF1 *fsig2 = new TF1("fsig2", "gaus", 20.0, 80.0);
+    fsig2->SetParameters(scale2, mean2.getVal(), sigma2.getVal());
+    TF1 *ftotal = new TF1("ftotal", "gaus(0) + gaus(3)", 20.0, 80.0);
+    ftotal->SetParameters(scale1, mean1.getVal(), sigma1.getVal(), scale2, mean2.getVal(), sigma2.getVal());
+    fsig1->SetLineColor(kRed);
+    fsig2->SetLineColor(kMagenta);
+    ftotal->SetLineColor(kBlack);
+    fsig1->SetLineStyle(2);
+    fsig2->SetLineStyle(2);
+    ftotal->SetLineStyle(1);
+    gStyle->SetOptStat(0);
+    hsig->Draw("E1");
+    ftotal->Draw("same");
+    fsig1->Draw("same");
+    fsig2->Draw("same");
+   
+
+    TLegend *leg3 = new TLegend(0.6, 0.75, 0.9, 0.9);
+    leg3->AddEntry(hsig, "Signal Data", "ep");
+    leg3->AddEntry(ftotal, "Fitted Total Signal", "l");
+    leg3->AddEntry(fsig1, "Fitted Signal 1", "l");
+    leg3->AddEntry(fsig2, "Fitted Signal 2", "l");
+    leg3->Draw();
+
+    cmain->Modified();
+    cmain->Update();
+    gPad->Update();
+
+    cmain->SaveAs("../Png_files/mixed_fit3.png");
+
+    ofstream out("../Txt_files/mixed_fit3_results.txt");
+
+    out << "Fit Results:" << endl;
+    out << "Signal Yield: " << nsig.getVal() << " ± " << nsig.getError() << endl;
+    out << "Mean1: " << mean1.getVal() << " ± " << mean1.getError() << endl;
+    out << "Width1: " << sigma1.getVal() << " ± " << sigma1.getError() << endl;
+    out << "Mean2: " << mean2.getVal() << " ± " << mean2.getError() << endl;
+    out << "Width2: " << sigma2.getVal() << " ± " << sigma2.getError() << endl;
+    out << "Signal Fraction: " << frac.getVal() << " ± " << frac.getError() << endl;
+    out << "Background Yield: " << nbkg.getVal() << " ± " << nbkg.getError() << endl;
+
+    out.close();
+}
